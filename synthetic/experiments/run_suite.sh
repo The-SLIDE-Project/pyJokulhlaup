7b1d46d84b569845ac5c78b4ea1d5952dc0d9f16
adamjhepburn <adamjhepburn@gmail.com>
2025-06-09 16:05:14 +0100

    added run_suite

---
+++ synthetic/experiments/run_suite.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# The actual (serial) computation, for a default model run
+
+row_count=$(($(wc -l < './suite_parameters.csv') - 1))
+for i in $(seq 1 $row_count); do
+    eval "python3.12 -m src.run_job './suite_parameters.csv' $i"
+done
+
