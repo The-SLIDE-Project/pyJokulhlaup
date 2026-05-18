2285a71580dab36f7798ece94f781870509bc83a
adam on workstation ernie <adamjhepburn@gmail.com>
2026-05-18 09:44:34 +0100

    making supp movie

--- synthetic/experiments/run_default.sh
+++ synthetic/experiments/run_default.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # The actual (serial) computation, for a default model run
 
-eval "python3.12 -m src.run_job './suite_parameters.csv' 25"
+eval "python3.12 -m src.run_job './suite_parameters.csv' 1"
 
